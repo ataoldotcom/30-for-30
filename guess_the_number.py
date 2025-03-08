59a74ccb5d5c2e7c3ceab8ee02b22fa4e61059bf
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-08 00:29:59 -0600

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -4,6 +4,7 @@
 import random
 
 start = "What is your name?"
+loop_game = "That's not it.\nWould you like to guess again: \'Yes\' or \'No\'?"
 print(start)
 name = input()
 
@@ -12,10 +13,30 @@ mystery_number = random.randint(1,10)
 
 #print(mystery_number)
 
+
 while True:
+    latest_guess = int(input("Alright {name}, take your guess!"))
+
+    if latest_guess == mystery_number:
+        print("Winner! Winner! chicken dinner!")
+        break
+    # below code needs to be finished. exception thrown  when user puts in a non numeric value
+    elif latest_guess =
+        # need to add a line break between "{name}." and "So".
+        print("Numbers only {name}.\nSo close!! That is a shape 💕") 
+    else: 
+        print(loop_game)
+        cont_loop = input().lower
+        if cont_loop.lower() == "yes":
+            print("No problemo. Thanks for playing")
+            break
+
+
+
+
 
 ###turn to Loop
-print("Please guess the number")
-user_latest_guess = int(input())
+##print("Please guess the number")
+##user_latest_guess = int(input())
 
-print(user_latest_guess)
+##print(user_latest_guess)
