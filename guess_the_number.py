54086f6434621bf7d85465cf38dc51976fe9484d
Alex B <78866932+ataoldotcom@users.noreply.github.com>
2025-03-08 02:03:53 -0600

    updated code. feature list incl

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,42 +1,50 @@
 #computer guess the number game
+    #need to add a count feature after every X amount of guesses
+    #add feature number reveal on guess_count = random.randint()
+    #add a user database and leaderboard of high scores
+    #add a time element
+
+
 
 #rng (random number generator)
 import random
 
 start = "What is your name?"
-loop_game = "That's not it.\nWould you like to guess again: \'Yes\' or \'No\'?"
+loop_game = "..would you like to go again: \'Yes\' or \'No\'?"
 print(start)
 name = input()
 
-print(f"Hello {name}, I am thinking of a number between 1 and 10")
-mystery_number = random.randint(1,10)
-
-#print(mystery_number)
-
-
 while True:
-    latest_guess = int(input("Alright {name}, take your guess!"))
 
-    if latest_guess == mystery_number:
-        print("Winner! Winner! chicken dinner!")
-        break
-    # below code needs to be finished. exception thrown  when user puts in a non numeric value
-    elif latest_guess =
-        # need to add a line break between "{name}." and "So".
-        print("Numbers only {name}.\nSo close!! That is a shape 💕") 
-    else: 
-        print(loop_game)
-        cont_loop = input().lower
-        if cont_loop.lower() == "yes":
-            print("No problemo. Thanks for playing")
+    print(f"Hello {name}, I am thinking of a number between 1 and 10")
+    mystery_number = random.randint(1,10)
+
+    #print(mystery_number)
+
+
+    while True:
+        #turned into exception handling
+        try:
+            latest_guess = int(input(f"Alright, take your guess! \n"))
+        except ValueError:
+            print(f"\"So close!! That is a shape 💕.\" Numbers only I'm afraid {name}.") 
+            continue
+        if latest_guess == mystery_number:
+            print("Winner! Winner! chicken dinner!\nYou can end on a winning streak.")
+            print("unless..", end="")
             break
+        # added hints
+        if latest_guess > mystery_number:
+            print("Sorry, your're too high.")
 
+        else:
+            print("Sorry, you're too low.")
+    # moved the loop_game prompt to its own loop for effeciency
+    print(loop_game)
+    cont_loop = input().lower()[:2]
+    if cont_loop == "no":
+        print("No problemo. Thanks for playing.")
+        break
+    else:
+        continue
 
-
-
-
-###turn to Loop
-##print("Please guess the number")
-##user_latest_guess = int(input())
-
-##print(user_latest_guess)
