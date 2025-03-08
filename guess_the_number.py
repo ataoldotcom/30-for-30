03d5b30e4a17244cd8d7f95c36cb52f3bc6e3069
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-07 23:59:14 -0600

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,15 +3,19 @@
 #rng (random number generator)
 import random
 
-print("What is your name?")
+start = "What is your name?"
+print(start)
 name = input()
 
-print(f"hello {name}, I am thinking of a number between 1 and 10")
+print(f"Hello {name}, I am thinking of a number between 1 and 10")
 mystery_number = random.randint(1,10)
 
 #print(mystery_number)
 
+while True:
 
-def computer_guess(x):
-    low = 1
-    high = x
+###turn to Loop
+print("Please guess the number")
+user_latest_guess = int(input())
+
+print(user_latest_guess)
