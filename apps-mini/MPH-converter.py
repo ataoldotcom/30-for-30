0b558739e474fe59444d03c6e3befec3a1f424c7
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-11 00:29:48 -0500

    fixed rounding issues. formatting

--- apps-mini/MPH-converter.py
+++ apps-mini/MPH-converter.py
@@ -1,11 +1,14 @@
-print("Welcome to the MPH Conversion app\n")
+print("\n\nWelcome to the MPH Conversion app\n")
 
 while True:
     try:
-        speed = float(input("What is your speed in miles per hour: \n"))
+        speed = float(input("What is your speed in Miles per hour: \n"))
     except ValueError:
         print("Please enter a valid speed")
         continue
     else:
-        conversion = speed * 1.6094
-        print(f"Your converted speed is: {conversion}")
+        #this is still returning precision float
+        #conversion = round(speed * 1.6094,2)
+        conversion = (speed * 1.6094)
+        print(f"Your speed in Kilometers per hour:\n{round(conversion,2)} Km/H\n")
+        break
