9bd5243992a866e6dacbc2a4789a065af6f1da3a
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-11 01:09:14 -0500

    temp converter finish functions

---
+++ apps-mini/temp_conv.py
@@ -0,0 +1,21 @@
+## Convert a temperature collected in farenheit to Celcius + Kelvin
+## to change this up from the mph one, use functions.
+
+print("\nWelcome to the Temp Converter\n")
+
+
+def f_to_c(us_temp):
+    #enter fomula
+    return round(,4)
+def c_to_k()
+
+
+#try-except error handling
+while True:    
+    try:
+        temp_us = float(input("What is the temperature in farenheit: \n"))
+        celsius_conv = (temp_us * )
+        kelv_conv = ()
+
+    except ValueError:
+        print("Please enter a valid numeric temperature")
