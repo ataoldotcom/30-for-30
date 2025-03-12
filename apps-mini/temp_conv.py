ed16bf5d20a076c7c8ce48780f359b611d07739b
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-11 20:43:49 -0500

    working code

--- apps-mini/temp_conv.py
+++ apps-mini/temp_conv.py
@@ -7,22 +7,26 @@ print("\nWelcome to the Temp Converter\n")
 def f_to_c(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return round(celsius,4)
-def c_to_k():
+def c_to_k(fahrenheit):
     #enter fomula
-    #kelvin = ()
+    kelvin = ((fahrenheit - 32) * 5/9) + 273.15
     return round(kelvin,4)
 
 #may need to move this in the loop
-temp_cel = f_to_c(temp_us)
-temp_k = ()
+#temp_cel = f_to_c(temp_us)
+#temp_k = c_to_k(temp_us)
 
 #try-except error handling
 while True:    
+    temp_us = round(float(input("What is the temperature in farenheit: \n")),2)
+    temp_cel = f_to_c(temp_us)
+    temp_k = c_to_k(temp_us)
+    
     try:
-        temp_us = round(float(input("What is the temperature in farenheit: \n")),2)
-        print("Degrees Fahreneheit: \n")
-        print("Degrees Celsius: \n")
-        print("Degrees Kelvin: \n")
+        #temp_us = round(float(input("What is the temperature in farenheit: \n")),2)
+        print(f"\nDegrees Fahreneheit: {temp_us}")
+        print(f"Degrees Celsius: {temp_cel}")
+        print(f"Degrees Kelvin: {temp_k}")
         break
 
     except ValueError:
