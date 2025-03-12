429f8983d06f2c73a26918797a2d27417241a76a
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-11 21:47:24 -0500

    formatting. fixing exception handling

--- apps-mini/temp_conv.py
+++ apps-mini/temp_conv.py
@@ -7,28 +7,27 @@ print("\nWelcome to the Temp Converter\n")
 def f_to_c(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return round(celsius,4)
+
 def c_to_k(fahrenheit):
-    #enter fomula
     kelvin = ((fahrenheit - 32) * 5/9) + 273.15
     return round(kelvin,4)
 
-#may need to move this in the loop
-#temp_cel = f_to_c(temp_us)
-#temp_k = c_to_k(temp_us)
-
-#try-except error handling
 while True:    
-    temp_us = round(float(input("What is the temperature in farenheit: \n")),2)
-    temp_cel = f_to_c(temp_us)
-    temp_k = c_to_k(temp_us)
+    #temp_us = round(float(input("Enter the temperature in farenheit: \n")),2)
+    #temp_cel = f_to_c(temp_us)
+    #temp_k = c_to_k(temp_us)
     
     try:
+        temp_us = round(float(input("Enter the temperature in degrees farenheit: \n")),2)
+        temp_cel = f_to_c(temp_us)
+        temp_k = c_to_k(temp_us)
+
         #temp_us = round(float(input("What is the temperature in farenheit: \n")),2)
-        print(f"\nDegrees Fahreneheit: {temp_us}")
-        print(f"Degrees Celsius: {temp_cel}")
-        print(f"Degrees Kelvin: {temp_k}")
+        print(f"\n{'Degrees Fahreneheit:':<20} {temp_us}")
+        print(f"{'Degrees Celsius:':<20} {temp_cel}")
+        print(f"{'Degrees Kelvin:':<20} {temp_k}\n")
         break
 
     except ValueError:
-        print("Please enter a valid numeric temperature")
+        print("Please enter a valid numeric temperature\n")
         continue
