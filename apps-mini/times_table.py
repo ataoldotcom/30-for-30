b2d0632ad5cba270cd01ccda3c656363db8a50f6
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-12 02:28:34 -0500

    not storing data. print only

--- apps-mini/times_table.py
+++ apps-mini/times_table.py
@@ -2,13 +2,25 @@
 
 print("Welcome to the Multiplication/Exponent Table App")
 
-name = input("What is your name:\t")
+name = input("What is your name:\t").strip().title()
+
+
+def table_m(numb):
+    for i in range (1,10):
+        print(f"\t {i} * {user_number} = {round(i * numb, 2)}")
+        #return round(i * numb, 2)
+
 
 while True:
+
+
     try:
-        number = float(input("What number would you like to work with?:\t"))
-        print(f"\nMultiplication Table or {number}\n")
+        user_number = float(input("What number would you like to work with?:\t"))
+        k = table_m(user_number)
+        print(f"\nMultiplication Table for {user_number}\n")
+        #print(k)
         #need to add print statements for math equations
+        # or create a cascading function x==0 (x =+ 1,9)  * number
         break
     except ValueError:
         print("Enter a numberic value only.")
