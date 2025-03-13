f6048fa7eafad22d42d711e32a8630af1a0e9d44
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-12 23:40:31 -0500

    finished. added formatting. fixed function.

--- apps-mini/times_table.py
+++ apps-mini/times_table.py
@@ -2,27 +2,43 @@
 ##need to change the code so that its storing results in a list. 
 ## fix the print station in the function if itend to RETURN results
 
-print("Welcome to the Multiplication/Exponent Table App")
+print("***\tWelcome to the Multiplication/Exponent Table App\t***\n")
 
-name = input("What is your name:\t").strip().title()
+name = input("\nWhat is your name:\t").strip().title()
 
 
 def table_m(numb):
-    for i in range (1,10):
-        print(f"\t {i} * {user_number} = {round(i * numb, 2)}")
-        #return round(i * numb, 2)
+    #can not append on a float
+    results = []
+    for i in range (1,12):
+        sol = round(i * numb, 2) 
+        #appending on list instead of sol. 
+        results.append(f"\t {f'{i} * {user_number}':<10}= {sol}")    
+    #return sol
+    return "\n".join(results)
 
+def table_ex(numb):
+    results_e = []
+    for i in range (1,12):
+        sol_e = round(numb ** i,2)
+        results_e.append(f"\t {f'{user_number} ** {i}':<10}= {sol_e}")
+    return "\n".join(results_e)
 
 while True:
 
 
     try:
-        user_number = float(input("What number would you like to work with?:\t"))
-        k = table_m(user_number)
+        user_number = float(input("\nWhat number would you like to work with?:\t"))
+        chart_m = table_m(user_number)
+        chart_ex = table_ex(user_number)
         print(f"\nMultiplication Table for {user_number}\n")
-        #print(k)
+        print(chart_m)
+        print(f"\nExponent Table for {user_number}\n")
+        print(chart_ex)
         #need to add print statements for math equations
         # or create a cascading function x==0 (x =+ 1,9)  * number
         break
     except ValueError:
         print("Enter a numberic value only.")
+
+print(f"\n****\tMath application complete, {name}!\t****\n")
