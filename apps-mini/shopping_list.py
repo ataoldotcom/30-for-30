31c2772a7aad4184bf434f38326389c5f22185f0
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-15 23:58:42 -0500

    numbered list

--- apps-mini/shopping_list.py
+++ apps-mini/shopping_list.py
@@ -1,18 +1,23 @@
 ## Grocerly list app
 ## feature list:
-## 1 print list out as numbered line items when > 5 items
+## print list out as numbered line items when > 5 items
 
 
 print("\n\n****\tWelcome To The Shopping List App\t****\n")
 print("Current Date and Time: ")
 
-list_grc = ["milk","oatmeal","salad","deoderant"]
+list_grc = ["milk","oatmeal","salad","deoderant", "socks", "potatoes"]
 
 while True: 
     if not list_grc:
         print("Your grocery list is currently empty. Lets get started!\n") 
         break
-    # possibly add elif #rd items
+    elif len(list_grc) > 5 :
+       #print("checking loop seq is working")
+       #need print numbered list
+        for i in range(len(list_grc)):
+            print('i. '.join([item.title() for item in list_grc]))
+            break       
     else:   
         print(f"You currently have: ({' and '.join([item.title() for item in list_grc])}) in your list.\n")
         break
