a05ee8fa55ccafa16217cd71373fcbf2bda3edf0
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-18 23:55:47 -0500

    created function of loops

--- apps-mini/shopping_list.py
+++ apps-mini/shopping_list.py
@@ -3,44 +3,49 @@
 ## If users attempts to add an out-of-stock item user is given an alert. 
 ##
 ## feature list:
-##done -  print list out as numbered line items when > 5 items
-## turn loop ( list formmatting) into function
-## turn loo (add user inputs into item) into function 
+## done -  print list out as numbered line items when > 5 items
+## done - turn loop ( list formmatting) into function
+## done - turn loop (add user inputs into item) into function 
 
 
 print("\n\n****\tWelcome To The Shopping List App\t****\n")
 print("Current Date and Time: ")
 
-list_grc = ["milk","oatmeal","salad","deoderant", "socks"]
+list_grc = ["milk","oatmeal","salad"]
 
 #loop determines formatting of printed list 
-while True: 
+
+def format_list(grocery_list): 
     #if list is empty prints: 
-    if not list_grc:
+    if not grocery_list:
         print("Your grocery list is currently empty. Lets get started!\n") 
-        break
     #if list has more than 5 items in it currently prints #. list
-    elif len(list_grc) > 5 :
-        print("Grocery List:")
-        for i,item in enumerate(list_grc, start=1):
-            print(f"{i}. {str(item.title())}")
-        break       
+    elif len(grocery_list) > 5 :
+        print(f"Grocery List: {len(list_grc)} items")
+        for i,item in enumerate(grocery_list, start=1):
+            print(f"{i}. {str(item.title())}")     
     #if list has < 5 items prints as "item + and + item"
     else:   
-        formatted_list = ' and '.join([item.title() for item in list_grc])
-        print(f"You currently have: ({formatted_list}) in your list.")
-        break
+        formatted_list = ' and '.join([item.title() for item in grocery_list])
+        print(f"You currently have {len(list_grc)} items: ({formatted_list}) in your list.")
 
+
+def add_items(grocery_list):
 #loop to add user inputs as items. 
-while True:
-    ans_add = input("Would you like to add more items to this list? ('Yes' or 'No'):\t").strip()
-    if ans_add[:2] == "no":
-        break
     while True:
-    # add items to list
-        user_item = input("\nEnter one item at a time to add to the grocery list \n\t--Or type \"Done\" when finished: ").strip().title()
-        if user_item == "Done":
+        ans_add = input("\nWould you like to add more items to this list? ('Yes' or 'No'):\t\n").strip()
+        if ans_add[:2] == "no":
+            print("\n", end="")
+            format_list(grocery_list)
+            #print("****\t\t\t\t\t\t****")
             break
-        list_grc.append(user_item)
+        while True:
+        # add items to list
+            user_item = input("Enter one item at a time to add to the grocery list \n\t--Or type \"Done\" when finished: ").strip().title()
+            if user_item == "Done":
+                break
+            grocery_list.append(user_item)
 
-print(list_grc)
+format_list(list_grc)
+add_items(list_grc)
+print("\n****\t\t\t\t\t\t****\n")
