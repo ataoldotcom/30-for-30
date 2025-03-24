8d05e1567c166d882d23eff0889752c5d422e6c4
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-24 00:06:15 -0500

    prototype removal function

--- apps-mini/shopping_list.py
+++ apps-mini/shopping_list.py
@@ -17,13 +17,13 @@ list_grc = ["milk","oatmeal","salad"]
 
 #loop determines formatting of printed list 
 
-def format_list(grocery_list): 
+def format_list_loop(grocery_list): 
     #if list is empty prints: 
     if not grocery_list:
         print("Your grocery list is currently empty. Lets get started!\n") 
     #if list has more than 5 items in it currently prints #. list
     elif len(grocery_list) > 5 :
-        print(f"Grocery List: {len(list_grc)} items")
+        print(f"Grocery List: {len(grocery_list)} items")
         for i,item in enumerate(grocery_list, start=1):
             print(f"{i}. {str(item.title())}")     
     #if list has < 5 items prints as "item + and + item"
@@ -38,7 +38,7 @@ def add_items(grocery_list):
         ans_add = input("\nWould you like to add more items to this list? ('Yes' or 'No'):\t\n").strip()
         if ans_add[:2] == "no":
             print("\n", end="")
-            format_list(grocery_list)
+            format_list_loop(grocery_list)
             #print("****\t\t\t\t\t\t****")
             break
         while True:
@@ -48,16 +48,35 @@ def add_items(grocery_list):
                 break
             grocery_list.append(user_item)
 
-format_list(list_grc)
+def numbered_list(grocery_list):
+    for i, item in enumerate(grocery_list, start= 1):
+        print(f"{i}. {str(item.title())}")
+
+format_list_loop(list_grc)
 add_items(list_grc)
 
 '''
-try:
-    to_remove = int(input("Enter the number of the item(s) you wish to remove: "))
-    xit = (input - 1)
-    removed_item = list_grc.pop(xit)
-except ValueError:
-    print("Please enter a number. If multiple inputs, comma seperate them eg:(3, 6, 9, ..) ")
+while True: 
+    try:
+        # ask user if they need to remove any items. 
+        if_remove = input("Would you like to remove any items from your list? Enter 'Yes' or 'No'").title()
+        if if_remove == "yes":
+            print("Below is your Shopping List")
+            numbered_list(grocery_list)
+            #exception handling to catch non numeric values. 
+            try:
+                #could turn the input of numbers into a list iteself. 
+                to_remove = int(input("Enter the number of the item you wish to remove: "))
+                xit = (to_remove - 1)
+                removed_item = list_grc.pop(xit)
+            except ValueError:
+                print("Please enter a valid number.")
+            except IndexError:
+                print("Please enter a number of an item in the list. If multiple inputs, comma seperate them eg:(3, 6, 9, ..) "
+
+    except :
+        print("Please type \"Yes\" or \"No\"")
+
 '''
 
 print("\n****\t\t\t\t\t\t****\n")
