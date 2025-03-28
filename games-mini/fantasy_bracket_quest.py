6f6b58b800d3f17cf49c7aaa8b718dd424c597bc
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2025-03-28 00:37:11 -0500

    hh

--- games-mini/fantasy_bracket_quest.py
+++ games-mini/fantasy_bracket_quest.py
@@ -1,4 +1,5 @@
 ## Connect to api to get player stats
 ## provide averages of stats
 ## Create list of random events. 
-#Have user input players and see how far they advance
+#Have user input players and see how far they advance in tour
+
